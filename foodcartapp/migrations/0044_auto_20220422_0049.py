22e51f910b56ca9df4feba122476b36a91852958
Anastasia <asya_apto@mail.ru>
2022-05-21 17:09:59 +0300

    Add .iterator() to avoid caching

--- foodcartapp/migrations/0044_auto_20220422_0049.py
+++ foodcartapp/migrations/0044_auto_20220422_0049.py
@@ -4,8 +4,8 @@ from django.db import migrations
 def get_prices(apps, schema_editor):
     Order = apps.get_model('foodcartapp', 'Order')
 
-    for order in Order.objects.all():
-        for product_qty in order.quantity.all():
+    for order in Order.objects.all().iterator():
+        for product_qty in order.quantity.all().iterator():
             price = product_qty.product.price
             product_qty.order_price = price
             product_qty.save()
@@ -13,8 +13,8 @@ def get_prices(apps, schema_editor):
 
 def move_backwards(apps, schema_editor):
     Order = apps.get_model('foodcartapp', 'Order')
-    for order in Order.objects.all():
-        for product_qty in order.quantity.all():
+    for order in Order.objects.all().iterator():
+        for product_qty in order.quantity.all().iterator():
             product_qty.order_price = None
             product_qty.save()
 
